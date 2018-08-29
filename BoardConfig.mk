35d4e892a9e9bf1e6dd5a92a59ca8e42fd526fdc
Nemirtingas <nanaki89@hotmail.fr>
2018-08-29 17:24:01 +0200

    Reworking device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,41 @@
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from ms01-common
+-include device/samsung/ms01-common/BoardConfigCommon.mk
+
+DEVICE_PATH := device/samsung/ms013g
+
+TARGET_OTA_ASSERT_DEVICE := ms013g
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# Audio
+AUDIO_FEATURE_ENABLED_MULTI_VOICE_SESSIONS := true
+AUDIO_FEATURE_SAMSUNG_DUAL_SIM := true
+
+# Radio
+SIM_COUNT := 2
+TARGET_GLOBAL_CFLAGS += -DANDROID_MULTI_SIM
+TARGET_GLOBAL_CPPFLAGS += -DANDROID_MULTI_SIM
+
+TARGET_KERNEL_CONFIG := lineageos_ms013g_defconfig
+ 
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
+#BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A7DEA0
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1866465280
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12562643968
+BOARD_CACHEIMAGE_PARTITION_SIZE := 218103808
